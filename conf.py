7dac0992c9d1e0b25c7facf12029dca7c6455ea5
Unknown <lavapower84@gmail.com>
2018-09-30 14:16:41 +0200

    Change theme (last)

--- conf.py
+++ conf.py
@@ -21,10 +21,6 @@ source_parsers = {
     '.md': CommonMarkParser,
 }
 
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 # -- Project information -----------------------------------------------------
 
 project = 'Sython'
@@ -82,7 +78,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
